3f716a794775d23bf0012ae151de4159d75a481b
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2021-01-01 20:33:50 +0700

    Menghapus fungsi perkenalan & email private
    Fungsi Perkenalan dihapus, Email yang private diubah manjadi protected

--- Aktor.py
+++ Aktor.py
@@ -25,38 +25,28 @@ class Kasir(Aktor):
 
     def getId(self):
         return self._id
+
     def getNama(self):
         return self._nama
 
     def getAlamat(self):
         return self._alamat
-
-    def perkenalan(self):
-        print("Pelanggan dengan Nomor Id {id}: \nNama saya {nama} dan alamat saya {alamat}".format(
-            id = self._id, nama = self._nama, alamat = self._alamat))
 
 class Pelanggan(Aktor):
     def __init__(self,id,nama,alamat,email):
         self._id = id
         self._nama = nama
         self._alamat = alamat
-        self.__email = email
+        self._email = email
 
     def getId(self):
         return self._id
+
     def getNama(self):
         return self._nama
 
     def getAlamat(self):
         return self._alamat
 
-    def perkenalan(self):
-        print("Pelanggan dengan Nomor Id {id}:\nNama saya {nama} dan alamat saya {alamat} \nDengan alamat email yaitu {email}".format(
-            id = self._id, nama = self._nama, alamat = self._alamat, email = self.__email))
-
-
-# renanta = Kasir(1920,"Renanta","Jember")
-# print(renanta.perkenalan())
-
-renanta = Pelanggan(3241,"Juna","JL. MA.Tamrin 45 Jakarta", "juna123@gamil.com")
-print(renanta.perkenalan())
+    def getEmail(self):
+        return self._email
