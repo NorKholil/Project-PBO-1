9de9f50a27f7d3c90033749743bed0b9c743c80e
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2021-01-02 12:50:29 +0700

    Update Database Elektronik
    Penambahan nama "elektronik"

--- Database Elektronik Revisi.py
+++ Database Elektronik Revisi.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect("Database XXX")
 c = conn.cursor()
 
 ###INI MEMBUAT TABEL###
-c.execute("""CREATE TABLE elektronik(nama_barang text, jumlah_barang integer, harga_barang integer, tgl_garansi text, no_seri text primary key) """)
+#c.execute("""CREATE TABLE elektronik(nama_barang text, jumlah_barang integer, harga_barang integer, tgl_garansi text, no_seri text primary key) """)
 
 elektronik1 = elektronik('Lenovo Ideapad Slim 3 ARE5', 5, 8500000, '15-02-2022', '12345678')
 elektronik2 = elektronik('Lenovo Ideapad Slim 5 ARE3', 4, 9000000, '15-02-2022', '23456781')
@@ -19,7 +19,7 @@ def menambah_elektronik(elektronik):
         c.execute("INSERT INTO elektronik VALUES (:nama_barang, :jumlah_barang, :harga_barang, :tgl_garansi, :no_seri)", 
         {'nama_barang': elektronik.nama_barang, 'jumlah_barang': elektronik.jumlah_barang, 'harga_barang': elektronik.harga_barang, 'tgl_garansi': elektronik.tgl_garansi, 'no_seri': elektronik.no_seri})
 
-def melihat_isi_tabel():
+def melihat_isi_tabel_elektronik():
     c.execute("SELECT * FROM elektronik")
     print(c.fetchall()) 
 
@@ -27,27 +27,27 @@ def melihat_elektronik_dari_nama(nama_barang):
     c.execute("SELECT * FROM elektronik WHERE nama_barang = :nama_barang", {'nama_barang': nama_barang})
     return c.fetchall()
 
-def update_nama_barang(elektronik, nama_barang):
+def update_nama_elektronik(elektronik, nama_barang):
     with conn:
         c.execute("""UPDATE elektronik SET nama_barang = :nama_barang
         WHERE no_seri = :no_seri""",
         {'nama_barang': nama_barang, 'jumlah_barang': elektronik.jumlah_barang, 'harga_barang': elektronik.harga_barang, 'tgl_garansi': elektronik.tgl_garansi, 'no_seri': elektronik.no_seri})
 
-def update_jumlah_barang(elektronik, jumlah_barang):
+def update_jumlah_elektronik(elektronik, jumlah_barang):
     with conn:
         c.execute("""UPDATE elektronik SET jumlah_barang = :jumlah_barang
         WHERE no_seri = :no_seri""",
         {'nama_barang': elektronik.nama_barang, 'jumlah_barang': jumlah_barang, 'harga_barang': elektronik.harga_barang, 'tgl_garansi': elektronik.tgl_garansi, 'no_seri': elektronik.no_seri})
 
 
-def update_harga_barang(elektronik, harga_barang):
+def update_harga_elektronik(elektronik, harga_barang):
     with conn:
         c.execute("""UPDATE elektronik SET harga_barang = :harga_barang
         WHERE no_seri = :no_seri""",
         {'nama_barang': elektronik.nama_barang, 'jumlah_barang': elektronik.jumlah_barang, 'harga_barang': harga_barang, 'tgl_garansi': elektronik.tgl_garansi, 'no_seri': elektronik.no_seri})
 
 
-def update_tgl_garansi(elektronik, tgl_garansi):
+def update_tgl_garansi_elektronik(elektronik, tgl_garansi):
     with conn:
         c.execute("""UPDATE elektronik SET tgl_garansi = :tgl_garansi
         WHERE no_seri = :no_seri""",
@@ -55,10 +55,8 @@ def update_tgl_garansi(elektronik, tgl_garansi):
 
 def hapus_elektronik(elektronik):
     with conn:
-        c.execute("DELETE from elektronik WHERE no_seri = :no_seri"
+        c.execute("DELETE from elektronik WHERE no_seri = :no_seri",
         {'no_seri': elektronik.no_seri})
-
-melihat_isi_tabel()
 
 conn.commit()
 
